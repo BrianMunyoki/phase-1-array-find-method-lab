f3c7cc835a6ce16e2293ddcc15d61fff82ffcd07
Brian Munyoki <Brianmunyoki168@gmail.com>
2025-06-30 15:04:30 +0300

    update

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  
+]
 function superbowlWin(array) {
   for (const record of array) {
     if (record.result === "W") {
